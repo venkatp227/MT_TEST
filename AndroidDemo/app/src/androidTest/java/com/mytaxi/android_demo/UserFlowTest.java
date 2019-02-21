269846dceaa0ead0240b1ed038c6a721fbea5aaf
Manuel <manuel.hunck@esailors.de>
2019-02-21 14:55:39 +0000

    added test

---
+++ AndroidDemo/app/src/androidTest/java/com/mytaxi/android_demo/UserFlowTest.java
@@ -0,0 +1,79 @@
+package com.mytaxi.android_demo;
+
+import android.content.Intent;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.rule.GrantPermissionRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
+import com.mytaxi.android_demo.activities.AuthenticationActivity;
+import com.mytaxi.android_demo.activities.MainActivity;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static org.hamcrest.Matchers.not;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import android.support.test.espresso.matcher.RootMatchers;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.is;
+
+//UserFlowTest  names it UserFlowTest since we have here a basic EndUserScenario
+@RunWith(AndroidJUnit4.class)
+public class UserFlowTest {
+
+    private static final String user="crazydog335";
+    private static final String pw="venture";
+    private static final String searchphrase="sa";
+    private static final String drivername="Sarah Scott";
+
+
+
+    @Rule
+    public ActivityTestRule<AuthenticationActivity> UserFlowTestRule = new ActivityTestRule<>(AuthenticationActivity.class, true, false);
+    public ActivityTestRule<MainActivity> UserFlowTest = new ActivityTestRule<>(MainActivity.class);
+    @Rule
+    public GrantPermissionRule permissionRule = GrantPermissionRule.grant(android.Manifest.permission.ACCESS_FINE_LOCATION);
+
+
+    @Test
+    public void logintoApp() throws Exception {
+        UserFlowTest.launchActivity(new Intent());
+        Thread.sleep(2000);
+        // instead of sleep something that waits for the elements to be visible to have if faster and stable
+        Log.println(Log.INFO, "@Test", "Enter user credentials");
+        onView(withId(R.id.edt_username))
+                .perform(typeText(user));
+        onView(withId(R.id.edt_password))
+                .perform(typeText(pw), closeSoftKeyboard());
+        onView(withId(R.id.btn_login))
+                .perform(click());
+        Log.println(Log.INFO, "logintoApp", "User " + user + " logged in");
+
+    }
+
+
+    @Test()
+    public void search_Driver() throws Exception {
+        UserFlowTest.launchActivity(null);
+        Thread.sleep(2000);
+        Log.println(Log.INFO, "@Test", "Test to search for a certain driver");
+
+        onView(withId(R.id.textSearch))
+                .perform(typeText(searchphrase), closeSoftKeyboard());
+
+        Thread.sleep(1000);
+        onView(withText(drivername))
+                .inRoot(RootMatchers.withDecorView(not(is(UserFlowTest.getActivity().getWindow().getDecorView()))))
+                .perform(scrollTo())
+                .perform(click());
+        onView(withId(R.id.fab))
+                .perform(click());
+
+    }
+
+}
